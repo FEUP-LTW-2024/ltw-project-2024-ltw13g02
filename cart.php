ab6642b3c3d924854e894bccf2f4f0f339621d29
Pedro Oliveira <up202206498@up.pt>
2024-04-20 22:24:35 +0100

    Shopping cart basic

--- cart.php
+++ cart.php
@@ -1,10 +1,10 @@
 <?php
     require_once('database/connection.php');
     require_once('templates/common.php');
+    require_once('templates/shopingCart.php');
+    output_header();
     $db = getDatabaseConnection();
-
-    draw_header();
-    draw_footer();
-
-
-?>
+    $items = getUserShopingCart($db, $_SESSION['idUser']);
+    output_cart_items($items);
+    //output_cart_price();
+    output_footer();
--- database/connection.php
+++ database/connection.php
@@ -14,4 +14,11 @@
     return $stmt->fetchAll();
   }
 
-?>
+  function getUserShopingCart( $db, $user_id ) {
+    $stmt = $db->prepare('SELECT P.prodName, P.prodDescription, P.price 
+                          FROM Product P JOIN ShoppingCart S ON P.idProduct = S.product 
+                          WHERE S.user = ? ');
+    $stmt->execute(array( $_SESSION['idUser']) );
+    $result = $stmt->fetchAll();
+    return $result;
+  }
