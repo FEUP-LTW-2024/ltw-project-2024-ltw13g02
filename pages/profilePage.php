3cd8a9abc2bff641957b357e24801a6f79c7d2a7
apilifogladif <filipajfidalgo@gmail.com>
2024-04-25 11:20:11 +0100

    design

--- pages/profilePage.php
+++ pages/profilePage.php
@@ -16,12 +16,6 @@
 
 <!DOCTYPE html>
 <html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Page Title</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
 <body>
 <main class="center-text">
     <div class="user-info">
