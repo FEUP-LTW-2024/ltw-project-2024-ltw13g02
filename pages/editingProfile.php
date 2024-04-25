b45920287c2a7b3edb1cf33494c5a91bbbaec694
Leonor2004 <up202205796@up.pt>
2024-04-25 20:33:50 +0100

    first draft of editing profile and my announcements

--- pages/editingProfile.php
+++ pages/editingProfile.php
@@ -13,3 +13,30 @@
   drawHeader($session);
   drawHamburguer($session);
 ?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<body>
+<main class="center-text">
+    <div class="user-info">
+        <?php $email = $session->getEmail(); ?>
+        <div class="info">
+        <?php
+          if($email != null) {
+        ?>
+            <h2><?php echo "First Name: " . $session->getFirstName(); ?></h2>
+            <h2><?php echo "Last Name: " . $session->getLastName(); ?></h2>
+            <h2><?php echo "Email: " . $email = $session->getEmail(); ?></h2>
+            <h2><?php echo "Phone: " . $session->getPhone(); ?></h2>
+            <h2><?php echo "Country: " . $session->getCountry(); ?></h2>
+            <h2><?php echo "City: " . $session->getCity(); ?></h2>
+            <h2><?php echo "Address: " . $session->getAddress(); ?></h2>
+            <h2><?php echo "zipCode: " . $session->getZipCode(); ?></h2>
+        <?php
+          }
+        ?>
+        </div>
+    </div>
+  </body>
+</html>
+
--- pages/myAnnouncements.php
+++ pages/myAnnouncements.php
@@ -19,9 +19,31 @@
   <body>
     <main>
       <div class="user-info">
-        <h2><?php echo $session->getEmail(); ?></h2>
-        <h2> my announcements </h2>
-        <h2><?php echo $session->getFirstName() . " " . $session->getLastName(); ?></h2>
+        <section class="Products" id="Announcements">
+            <h2>My Announcements</h2>
+            <article>
+                <!-- PHP fica aqui -->
+                <div class="offers_container">
+                    <div class="offer">A1</div>
+                    <div class="offer">A2</div>
+                    <div class="offer">A3</div>
+                    <div class="offer">A4</div>
+                    <div class="offer">A5</div>
+                    <div class="offer">A6</div>
+                    <div class="offer">A7</div>
+                    <div class="offer">A8</div>
+                    <div class="offer">A9</div>
+                </div>
+                <!--<input type="submit" value="&#60" class="MovArrows" >
+                <input type="submit" value=">" class="MovArrows">-->
+            </article>
+
+            <!-- PHP fica aqui -->
+            <!--
+            <input type="submit" value="Move Left"class="MovArrows">
+            <input type="submit" value="Move Right" class="MovArrows">
+            -->
+        </section>
       </div>
     </main>
 
