584c1542dcc1bf1983c39fb6b4f3cca6b36a503d
apilifogladif <filipajfidalgo@gmail.com>
2024-05-04 16:09:28 +0100

    changing photos

--- templates/product_tmpl.php
+++ templates/product_tmpl.php
@@ -8,6 +8,8 @@ require_once(__DIR__ . '/../database/get_from_db.php');
 
 require_once(__DIR__ . '/../database/change_in_db.php');
 
+require_once(__DIR__ . '/../vendor/autoload.php');
+
 ?>
 
 <?php function drawProductHeader(Session $session, $idProduct) { 
@@ -21,18 +23,33 @@ require_once(__DIR__ . '/../database/change_in_db.php');
 
 <?php function drawProduct(Session $session, $idProduct) { 
     $product = getProduct($idProduct); 
-    $photos = getPhotos($idProduct);?>
+    $photos = getPhotos($idProduct); ?>
     <div class="product-grid">
         <div class="product-image-container">
             <?php if (count($photos) == 0) { ?>
-                <img class="product-image" src="../images/products/no_images_big.png" alt="Photo">
+                <img id="product-image" src="../images/products/no_images_big.png" alt="Photo">
             <?php } else { ?>
-                    <img src="../images/products/<?php echo $photos[0]; ?>" alt="<?php echo $photos[0]['alt']; ?>">
+                <img id="product-image" src="../images/products/<?php echo $photos[0]['photo']; ?>" alt="photo">
             <?php } ?>
         </div>
-        <?php if (count($photos) != 0) { ?>
-            <button class="prev-button" ><i class="fa fa-angle-left fa-2x"></i></button>
-            <button class="next-button" ><i class="fa fa-angle-right fa-2x"></i></button>
+        <?php if (count($photos) > 1) { ?>
+            <button class="prev-button" onclick="changePhoto(-1)"><i class="fa fa-angle-left fa-2x prev-icon"></i></button>
+            <button class="next-button" onclick="changePhoto(1)"><i class="fa fa-angle-right fa-2x next-icon"></i></button>
         <?php } ?>
     </div>
+
+    <script>
+        var currentIndex = 0;
+        var photos = <?php echo json_encode($photos); ?>;
+
+        function changePhoto(delta) {
+            currentIndex += delta;
+            if (currentIndex < 0) {
+                currentIndex = photos.length - 1;
+            } else if (currentIndex >= photos.length) {
+                currentIndex = 0;
+            }
+            document.getElementById('product-image').src = "../images/products/" + photos[currentIndex]['photo'];
+        }
+    </script>
 <?php } ?> 
