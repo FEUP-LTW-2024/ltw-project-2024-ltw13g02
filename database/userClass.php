51d2f19dbe8fa0cd34dcb0cf2688e6bbfd4603f3
apilifogladif <filipajfidalgo@gmail.com>
2024-05-11 17:01:04 +0100

    update

--- database/userClass.php
+++ database/userClass.php
@@ -1,7 +1,6 @@
 <?php
 require_once(__DIR__ . '/chatClass.php');
 require_once(__DIR__ . '/../database/connection_to_db.php');
-require_once(__DIR__ . '/../vendor/autoload.php');
 
 class User {
     private string $idUser;
