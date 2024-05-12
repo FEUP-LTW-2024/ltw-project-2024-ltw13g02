f645caad0a6dca8986c1ce2fafd6b3c53b6c9706
apilifogladif <filipajfidalgo@gmail.com>
2024-05-12 14:30:56 +0100

    update

--- api/api_products.php
+++ api/api_products.php
@@ -4,7 +4,7 @@
   require_once(__DIR__ . '/../utils/session.php');
   $session = new Session();
 
-  require_once(__DIR__ . '/../database/connection.db.php');
+  require_once(__DIR__ . '/../database/connection_to_db.php');
   require_once(__DIR__ . '/../database/productClass.php');
 
   $db = getDatabaseConnection();
