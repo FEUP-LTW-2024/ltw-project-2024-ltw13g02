8d70b6e4953d68f834f7266fd24db38aa05e4a71
apilifogladif <filipajfidalgo@gmail.com>
2024-05-14 10:24:29 +0100

    update

--- javascript/search.js
+++ javascript/search.js
@@ -22,72 +22,25 @@ if (searchProducts) {
         for (const product of products) {
             const div = document.createElement('div');
             div.className = "static_offer_container";
-
-            fetchProductSeller(product.seller)
-                .then(seller => {
-                    const sellerLink = document.createElement('a');
-                    sellerLink.href = '../pages/seller_page.php?user=' + seller.id;
-                    sellerLink.className = "user_small_card";
-                
-                    const sellerImage = document.createElement('img');
-                    sellerImage.className = "user_small_pfp";
-                    if (seller.photo !== "Sem FF") {
-                        sellerImage.src = '../images/userProfile/' + seller.photo;
-                    } else {
-                        const userIcon = document.createElement('i');
-                        userIcon.className = "fa fa-user fa-1x user-icons";
-                        sellerLink.appendChild(userIcon);
-                    }
-                
-                    const sellerName = document.createElement('p');
-                    sellerName.textContent = seller.firstName + " " + seller.lastName;
-                
-                    sellerLink.appendChild(sellerImage);
-                    sellerLink.appendChild(sellerName);
-                    div.appendChild(sellerLink);
-
-                    const productLocation = document.createElement('h5');
-                    productLocation.textContent = product.city + ", " + product.country;
-                    div.appendChild(productLocation);
-                })
-                .catch(error => {
-                    console.error('Error fetching seller info:', error);
-                    // Handle error
-                });
-
-            fetchProductPhotos(product.id)
-                .then(photos => {
-                    const productLink = document.createElement('a');
-                    productLink.href = '../pages/productPage.php?product=' + product.id;
         
-                    const productImage = document.createElement('img');
-                    productImage.className = "offer_img";
-                    productImage.src = '../images/products/' + photos[0];
+            try {
+                const seller = await fetchProductSeller(product.seller);
+                const sellerLink = document.createElement('a');
+                // Create seller link and append it to div
         
-                    productLink.appendChild(productImage);
-                    div.appendChild(productLink);
-                })
-                .catch(error => {
-                    console.error('Error fetching product photos:', error);
-                    // Handle error
-                });
-
-            const offerInfo = document.createElement('div');
-            offerInfo.className = "offer_info";
-
-            const productName = document.createElement('h4');
-            productName.textContent = product.name.substring(0, 30);
-
-            const productPrice = document.createElement('p');
-            productPrice.textContent = product.price + "€";
-
-            offerInfo.appendChild(productName);
-            offerInfo.appendChild(productPrice);
-
-            div.appendChild(offerInfo);
-
+                const photos = await fetchProductPhotos(product.id);
+                const productLink = document.createElement('a');
+                // Create product link and append it to div
+        
+                // Create and append offer info elements
+            } catch (error) {
+                console.error('Error:', error);
+                // Handle error
+            }
+        
             section.appendChild(div);
         }
+        
         
         async function fetchProductPhotos(productId) {
             try {
